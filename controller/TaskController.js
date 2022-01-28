b25012c7880061d5e2d9c630d44841485c850f32
codethi <docencia.thiago@gmail.com>
2022-01-28 11:41:59 -0300

    getbyid, update and delete

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -3,7 +3,7 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
   try {
     const tasksList = await Task.find();
-    return res.render("index", {tasksList});
+    return res.render("index", { tasksList, task: null, taskDelete: null });
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
@@ -15,7 +15,6 @@ const createTask = async (req, res) => {
   if (!task.task) {
     return res.redirect("/");
   }
-  
 
   try {
     await Task.create(task);
@@ -25,7 +24,44 @@ const createTask = async (req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  try {
+    const tasksList = await Task.find();
+    if (req.params.method == "update") {
+      const task = await Task.findOne({ _id: req.params.id });
+      res.render("index", { task, taskDelete: null, tasksList });
+    } else {
+      const taskDelete = await Task.findOne({ _id: req.params.id });
+      res.render("index", { task: null, taskDelete, tasksList });
+    }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const updateOneTask = async (req, res) => {
+  try {
+    const task = req.body;
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const deleteOneTask = async (req, res) => {
+  try {
+    await Task.deleteOne({ _id: req.params.id });
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
 module.exports = {
   getAllTasks,
   createTask,
+  getById,
+  updateOneTask,
+  deleteOneTask,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,8 @@ const TaskController = require("../controller/TaskController");
 
 routes.get("/", TaskController.getAllTasks);
 routes.post("/create", TaskController.createTask);
+routes.get("/getById/:id/:method", TaskController.getById);
+routes.post("/updateOne/:id", TaskController.updateOneTask);
+routes.get("/deleteOne/:id", TaskController.deleteOneTask);
 
 module.exports = routes;
