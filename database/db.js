e64116703cfa71d0459f89bbd795efa06e1bb598
codethi <docencia.thiago@gmail.com>
2022-02-28 17:39:26 -0300

    deploy configs

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDb = () => {
   mongoose
     .connect(
-      "mongodb+srv://root:admin@todolist.5d8yy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      process.env.DB_URI,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const path = require("path");
 const routes = require("./routes/routes");
@@ -5,7 +6,7 @@ const connectToDb = require("./database/db");
 
 connectToDb();
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
