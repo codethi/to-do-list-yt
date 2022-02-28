22747d039228d6a97c8400c8843437fb379e4b34
codethi <docencia.thiago@gmail.com>
2022-02-28 17:59:47 -0300

    port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const connectToDb = require("./database/db");
 
 connectToDb();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
