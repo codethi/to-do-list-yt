b4fed640ed7651c856464d71333c98a32649037e
codethi <docencia.thiago@gmail.com>
2022-02-28 17:51:57 -0300

    db

--- database/db.js
+++ database/db.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 
 const connectToDb = () => {
   mongoose
-    .connect(
-      process.env.DB_URI,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    )
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("MongoDB Atlas CONECTADO!"))
     .catch((err) => console.log(err));
 };
